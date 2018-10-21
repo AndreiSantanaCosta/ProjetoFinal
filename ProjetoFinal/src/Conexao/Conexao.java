8542e1aa0d211dbb1778882301d01458ae446b76
NygmaC <leonardo.crispin.oliveira00@gmail.com>
2018-10-21 15:58:54 -0200

    Classe de conexao

---
+++ ProjetoFinal/src/Conexao/Conexao.java
@@ -0,0 +1,35 @@
+package Conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+	static Connection conn = null;
+	
+	public Conexao() {
+
+	}
+
+	public static Connection getConexaoMYSQL() throws SQLException{
+		
+		DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+		String serverName = "localhost"; 
+		String mydatabase = "sistemacorporativo";
+		String porta = "3306";
+		String username = "root";  
+		String password = "toor"; 
+		String url = "jdbc:mysql://" + serverName +":" + porta + "/" + mydatabase+ "?useTimezone=true&serverTimezone=UTC&useSSL=false";
+			
+		conn = DriverManager.getConnection(url, username, password);	
+		return conn;
+	}
+	
+	public void closeConexaoMYSQL() {
+		try {
+			conn.close();
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+}
